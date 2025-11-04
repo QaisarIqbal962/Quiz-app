c75fbb5535cba8624f04493c104ff3df6c74a585
Qaisar iqbal <qaisarbangash47@gmail.com>
2025-11-04 12:23:13 +0500

    added some questions

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { questions } from "./data/questions";
 function App() {
   const [currentQuestion, setCurrentQuestion] = useState(0);
   return (
-    <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center " >
+    <div className="min-h-screen bg-gray-900 text-white flex flex-col  items-center justify-center " >
     <div className="text-center">
         <h1 className="text-4xl font-bold text-purple-600 mb-2">Quiz App</h1>
       <p className="text-gray-400">Test your knowledge</p> 
--- src/components/QuestionsCard.jsx
+++ src/components/QuestionsCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const QuestionsCard = ({data}) => {
     const {question,options,answer} = data;
   return (
-    <div>
+    <div className='bg-gray-800 p-6 rounded-2xl shadow-lg w-full max-w-xl border border-gray-700'>
     QuestionCard {question}
     </div>
   )
