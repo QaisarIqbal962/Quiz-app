b89b83faed134d1ce6fb01cd4bccc1076c4fd422
Qaisar iqbal <qaisarbangash47@gmail.com>
2025-11-04 14:50:32 +0500

    added Questionoptions

--- src/components/QuestionsCard.jsx
+++ src/components/QuestionsCard.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React from "react";
 
-const QuestionsCard = ({data}) => {
-    const {question,options,answer} = data;
+const QuestionsCard = ({ data }) => {
+  const { question, options, answer } = data;
   return (
-    <div className='bg-gray-800 p-6 rounded-2xl shadow-lg w-full max-w-xl border border-gray-700'>
-    QuestionCard {question}
+    <div className="bg-gray-800 p-6 rounded-2xl shadow-lg w-full max-w-xl border border-gray-700">
+      <p>{question}</p>
+      <div>
+        {options.map((option, index) => (
+          <button key={index}>{option}</button>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default QuestionsCard
+export default QuestionsCard;
